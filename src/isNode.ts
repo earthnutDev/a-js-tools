947276d93cc366f17c7c671a6a781899988cf284
earth nut dev <earthnut.dev@outlook.com>
2025-04-19 02:09:07 +0800

    version: 0.5.4-beta.0 2025-04-19 02:09:07 没啥，更新着玩 😄

--- src/isNode.ts
+++ src/isNode.ts
@@ -1,11 +1,12 @@
-import { isUndefined } from 'a-type-of-js';
 /**
  *
  * 判断当前环境是否为 node 环境
  *
  */
 export function isNode(): boolean {
-  return isUndefined(window) || isUndefined(window.document);
+  return (
+    typeof window === 'undefined' || typeof window.document === 'undefined'
+  );
 }
 
 /**
