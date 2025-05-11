b92dc74b282b3651cbea71af72050ce453d3b76c
earth nut dev <earthnut.dev@outlook.com>
2025-05-11 08:05:08 +0800

    修改配置文件

--- scripts/pub.sh
+++ scripts/pub.sh
@@ -1,27 +1,35 @@
 #!/bin/bash
 
+CHECK_VERSION="@qqi/check-version"
+# 安装  
+install_check_version() {
+    if ! npm  list -g --depth=0 | grep -q " ${CHECK_VERSION}"; then 
+        echo "当前未全局安装 '${CHECK_VERSION}'，即将进行安装"
+        npm install ${CHECK_VERSION} --global
+    else 
+         echo "包 ${CHECK_VERSION} 已全局安装"
+    fi
+}
 
+tag=""
+install_check_version
+if ! tag=$(npx "${CHECK_VERSION}" c=. 2>&1); then
+    echo "未通过版本校验：$tag"
+    exit 0
+fi
+echo "获取🉐发布标签为 ${tag}"
 # 依赖安装
 npm ci
-
-# 检测版本是否可用并获取标签
-tag=$(npx @qqi/check-version c=. 2>&1)
-exit_code=$?
-if [ $exit_code -ne 0 ]; then
-  echo "版本检测失败：$tag"
-  exit 1
-fi
-
 # 构建项目
 if ! npm run build; then 
   echo "构建失败" 
-  exit 1
+  exit 0
 fi
 
 # 切换到构建目录
 if [ ! -d "dist" ]; then 
   echo "未找到 dist 构建码"
-  exit 1
+  exit 0
 fi
 
 # 确保脚本在遇见错误时立即退出
